d701ede239a4150bcbfb0edbbaaf13c6b01d3b98
Peter <folimworld@gmail.com>
2017-04-20 14:45:50 +0900

    sudo

--- provisioning.sh
+++ provisioning.sh
@@ -18,7 +18,7 @@ sudo wget -qO- https://get.docker.com/ | sh
 
 # Docker-compose install
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
-chmod 755 /usr/bin/docker-compose
+sudo chmod 755 /usr/bin/docker-compose
 
 ## Get the dcs from docker
 curl -sL bit.ly/ralf_dcs -o ./dcs
