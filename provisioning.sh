e5968e075568527b0447f606f241b19067fa69f8
SKYul <folimworld@gmail.com>
2018-04-30 17:17:21 +0900

    Update provisioning.sh
    docker-ce stable version

--- provisioning.sh
+++ provisioning.sh
@@ -15,15 +15,15 @@ sudo $Pkg_mgmt install git -y
 sudo $Pkg_mgmt install bc -y
 #sudo $Pkg_mgmt update -y
 
-# Docker install
-sudo wget -qO- https://get.docker.com/ | sh
+# Docker stable ver. install 
+sudo wget -qO- https://get.docker.com/ | CHANNEL=stable sh
 
 # Docker-compose install
-#curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
 sudo chmod 755 /usr/bin/docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo systemctl status docker |grep Active
 
 ## Get the dcs from docker
 curl -sL bit.ly/ralf_dcs -o ./dcs
